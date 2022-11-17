336ffa77c768fb636ca9fd0667e70f9d74b19744
nikita.klyutkin@gmail.com <nikita.klyutkin@gmail.com>
2022-11-17 16:39:30 +0300

    add new in program

--- Program.cs
+++ Program.cs
@@ -5,3 +5,4 @@ var result = ObjectExtentions.ReadFromJsonFile<Squad>(@"C:/squad.json");
 ObjectExtentions.WriteToJsonFile<Squad>(@"C:/result.json", result);
 ObjectExtentions.WriteToXmlFile<Squad>(@"C:/resultxml.xml", result);
 var xmlresult = ObjectExtentions.ReadFromXmlFile<Squad>(@"C:/resultxml.xml");
+ObjectExtentions.WriteToJsonFile<Squad>(@"C:/xmlTOjson.json", xmlresult);
