00fbf70dbeb9f7fc0003d1ea2e7f8ea2888b573a
nikita.klyutkin@gmail.com <nikita.klyutkin@gmail.com>
2022-11-17 12:02:21 +0300

    Add project files.

---
+++ ObjectExtentions.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Lesson13Serialization
+{
+    public static class ObjectExtentions
+    {
+        public static T? ReadFromJsonFile<T>(string filePath) where T : new()
+        {
+            TextReader reader = null;
+            try
+            {
+                reader = new TextReader(filePath);
+                var fileContents = reader.ReadToEnd();
+                return JsonConvert.DeserializeObject<T>(fileContents);
+            }
+            finally
+            {
+                if (reader != null)
+                    reader.Close();
+            }
+        }
+    }
+}
---
+++ Squad.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson13Serialization
+{
+    public class Squad
+    {
+        public string squadName { get; set; }
+        public string homeTown { get; set; }
+        public int formed { get; set; }
+        public string secretBase { get; set; }
+        public bool active { get; set; }
+        public List<Members> members { get; set; }
+        
+    }
+
+    public class Members
+    {
+        public string name { get; set; }
+        public int age { get; set; }
+        public string secretIdentity { get; set; }
+        public List<string> powers { get; set; }
+
+    }
+}
