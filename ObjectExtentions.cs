1620b60fceac7090de509fd92efa65f3b27df7eb
nikita.klyutkin@gmail.com <nikita.klyutkin@gmail.com>
2022-11-17 16:33:20 +0300

    add try open file

--- ObjectExtentions.cs
+++ ObjectExtentions.cs
@@ -1,6 +1,7 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Linq;
 using System.Text;
 using System.Text.Json.Serialization;
@@ -11,23 +12,25 @@ namespace Lesson13Serialization
 {
     public static class ObjectExtentions
     {
-        //public static T? ReadFromJsonFile<T>(string filePath) where T : new()
-        //{
-        //    TextReader reader = null;
-        //    try
-        //    {
-        //        reader = new TextReader(filePath);
-        //        var fileContents = reader.ReadToEnd();
-        //        return JsonConvert.DeserializeObject<T>(fileContents);
-        //    }
-        //    finally
-        //    {
-        //        if (reader != null)
-        //            reader.Close();
-        //    }
-        //}
+        public static void TryOpenFile<T>(string filePath) where T : new()
+        {
+            TextReader reader = null;
+            try
+            {
+                reader = new StreamReader(filePath);
+                if(reader != null)
+                    reader.Close();
+            }
+            catch
+            {
+                if (reader == null)
+                    throw new WrongNoFile("Нет файла по этому пути");
+            }
+        }
+
         public static void WriteToJsonFile<T>(string filePath, T objectToWrite, bool append = false) where T : new()
         {
+            
             TextWriter writer = null;
             try
             {
@@ -43,6 +46,7 @@ namespace Lesson13Serialization
         }
         public static T ReadFromJsonFile<T>(string filePath) where T : new()
         {
+            TryOpenFile<T>(filePath);
             TextReader reader = null;
             try
             {
@@ -50,6 +54,7 @@ namespace Lesson13Serialization
                 var fileContents = reader.ReadToEnd();
                 return JsonConvert.DeserializeObject<T>(fileContents);
             }
+
             finally
             {
                 if (reader != null)
@@ -69,6 +74,34 @@ namespace Lesson13Serialization
             {
                 if (writer != null)
                     writer.Close();
+            }
+        }
+        public static T ReadFromXmlFile<T>(string filePath) where T : new()
+        {
+            TryOpenFile<T>(filePath);
+            TextReader reader = null;
+            try
+            {
+                var serializer = new XmlSerializer(typeof(T));
+                reader = new StreamReader(filePath);
+                return (T)serializer.Deserialize(reader);
+            }
+            finally
+            {
+                if (reader != null)
+                    reader.Close();
+            }
+        }
+
+        public class WrongNoFile: Exception
+        {
+            public WrongNoFile()
+            {
+
+            }
+            public WrongNoFile(string message) : base(message)
+            {
+                Console.WriteLine(message);
             }
         }
     }
--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿
 using Lesson13Serialization;
-
 var result = ObjectExtentions.ReadFromJsonFile<Squad>(@"C:/squad.json");
 
 ObjectExtentions.WriteToJsonFile<Squad>(@"C:/result.json", result);
 ObjectExtentions.WriteToXmlFile<Squad>(@"C:/resultxml.xml", result);
+var xmlresult = ObjectExtentions.ReadFromXmlFile<Squad>(@"C:/resultxml.xml");
